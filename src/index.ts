a8a5701ff8b52c7a406e26d818344a104511fe0f
Xingchen Hong <hello@xc-h.com>
2020-01-28 14:53:08 -0800

    Add prettier, jest and format.

--- src/index.ts
+++ src/index.ts
@@ -5,115 +5,103 @@ const whitespaceRegEx = new RegExp('\\s');
  * Remove all extra whitespace, including in the middle of the string.
  * @param str
  */
-function trimString
-(
-  str : string
-) : string
-{
-  if (whitespaceRegEx.test(str)) {
-    return str
-    .replace(whitespaceRegEx, delimiter)
-    .split(delimiter)
-    .filter(isNonEmptyString)
-    .join(delimiter);
-  }
+function trimString(str: string): string {
+    if (whitespaceRegEx.test(str)) {
+        return str
+            .replace(whitespaceRegEx, delimiter)
+            .split(delimiter)
+            .filter(isNonEmptyString)
+            .join(delimiter);
+    }
 
-  return str;
+    return str;
 }
 
 /**
  * Returns false if the string is empty  or contains only white spaces.
  * @param str
  */
-function isNonEmptyString
-(
-  str : string
-) : boolean
-{
-  const isEmpty = trimString(str).length == 0;
-  return !isEmpty;
+function isNonEmptyString(str: string): boolean {
+    const isEmpty = trimString(str).length == 0;
+    return !isEmpty;
 }
 
 /**
  * Return value is trimmed.
  * @param value
  */
-function getClassNameFromSingleValue
-(
-  value : any
-) : string
-{
-  // For `undefined`, null, false, 0, ''.
-  if (!value) {
-    return '';
-  }
-
-  // Ignore meaningless value `true`.
-  if (value == true) {
-    return '';
-  }
-
-  if (Array.isArray(value)) {
-    // For `[]`.
-    if (value.length == 0) {
-      return '';
+function getClassNameFromSingleValue(value: any): string {
+    // For `undefined`, null, false, 0, ''.
+    if (!value) {
+        return '';
     }
 
-    const values : Array<string> = value
-    .map(getClassNameFromSingleValue)
-    .filter(isNonEmptyString);
-
-    return trimString(values.join(delimiter));
-  }
-
-  const type : string = typeof value;
-
-  // Numbers are not valid class names.
-  if (type == 'number') {
-    return '';
-  }
-
-  if (type == 'function') {
-    return getClassNameFromSingleValue(value());
-  }
-
-  if (type == 'object') {
-    // If object has string form, use it.
-    if ('toString' in value && value.toString != Object.prototype.toString) {
-      // Don't trust the return value of `value.toString()`.
-      const potentialStringValue : any = value.toString();
-
-      return getClassNameFromSingleValue(potentialStringValue);
+    // Ignore meaningless value `true`.
+    if (value == true) {
+        return '';
     }
 
-    // Otherwise collect property names that have truthy value.
-    const propNames = Object.keys(value);
+    if (Array.isArray(value)) {
+        // For `[]`.
+        if (value.length == 0) {
+            return '';
+        }
 
-    const truthyPropNames = propNames
-    .filter((propName) => {
-      let propValue : any = value[propName];
-      const propValueType : string = typeof propValue;
+        const values: Array<string> = value
+            .map(getClassNameFromSingleValue)
+            .filter(isNonEmptyString);
 
-      if (propValueType == 'function') {
-        propValue = propValue();
-      }
+        return trimString(values.join(delimiter));
+    }
 
-      return Boolean(propValue);
-    })
-    .filter(isNonEmptyString);
+    const type: string = typeof value;
 
-    return truthyPropNames.join(delimiter).trim();
-  }
+    // Numbers are not valid class names.
+    if (type == 'number') {
+        return '';
+    }
 
-  // For all else, return its trimmed string form.
-  return trimString(String(value));
+    if (type == 'function') {
+        return getClassNameFromSingleValue(value());
+    }
+
+    if (type == 'object') {
+        // If object has string form, use it.
+        if (
+            'toString' in value &&
+            value.toString != Object.prototype.toString
+        ) {
+            // Don't trust the return value of `value.toString()`.
+            const potentialStringValue: any = value.toString();
+
+            return getClassNameFromSingleValue(potentialStringValue);
+        }
+
+        // Otherwise collect property names that have truthy value.
+        const propNames = Object.keys(value);
+
+        const truthyPropNames = propNames
+            .filter((propName) => {
+                let propValue: any = value[propName];
+                const propValueType: string = typeof propValue;
+
+                if (propValueType == 'function') {
+                    propValue = propValue();
+                }
+
+                return Boolean(propValue);
+            })
+            .filter(isNonEmptyString);
+
+        return truthyPropNames.join(delimiter).trim();
+    }
+
+    // For all else, return its trimmed string form.
+    return trimString(String(value));
 }
 
-function classNames
-(
-  ...items : Array<any>
-) : string {
-  return getClassNameFromSingleValue(items);
+function classNames(...items: Array<any>): string {
+    return getClassNameFromSingleValue(items);
 }
 
 export default classNames;
